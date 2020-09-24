e3eb5b2b69a3df5d9270974a17cca59b51cf1eaf
Creed Camp <creedomed@gmail.com>
2020-09-24 09:04:52 -0700

    saving15

--- main-cc.js
+++ main-cc.js
@@ -1,6 +1,5 @@
-//step 1 create grid//done
-//step 2 insert items from items.js into each section and have them run 1fr//not done
-//
+//step 1 create a .map on the values inside of the array items.
+//step 2 return it to index.html
 //maybe try nesting inside of an all-encompasing function
 const big = document.getElementById("items-container")
 //attempted to filter for free shipping, but returned to much data, and returned it to wrong items as well.
@@ -13,7 +12,7 @@ const arr = items.results
     return `<div class="bla">
     <a href="${item.url}" target="_blank">
     <img class="images" src = "${item.Images[0].url_170x135}"/>
-    <h3>${item.title}</h3>
+    <h3 style= "width: 48%;">${item.title}</h3>
     <p>From ${item.Shop.shop_name}</p>
     <p><div><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i></div>, views${item.views}</p>
     <p>$${item.price}</p></a>
